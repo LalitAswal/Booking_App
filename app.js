3d3e0d5fa3df7c49957d76ae473d19fb64398ac4
LalitAswal <LalitAswal143@gmail.com>
2023-08-09 18:59:45 +0530

    minor changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 //booking routes
 const bookingRoutes = require('./routes/bookingRoutes.js');
 
-//middlewares
+//middleware
 app.use(cors())
 app.use(express.json());
 // app.use(express.static('booking'))
